72d285a79b53ae5f8271c483b52cf91c45ba421c
Prisa <43382953+ElPrisa@users.noreply.github.com>
2018-10-30 08:59:13 +0100

    Update audio.py

--- audio.py
+++ audio.py
@@ -88,7 +88,7 @@ async def helper(ctx):
   await bot.say("**         !server-info** = info about Chicken server")
   await bot.say("**         !user-info** = info about user")
   await bot.say("**         !games** = list of all games that can be added to game updates ")
-  await bot.say("           If you need help with something else, write **!call** to call moderators or helpers")
+  await bot.say("If you need help with something else, write **!call** to call moderators or helpers")
 
 @bot.command(pass_context=True)
 async def call(ctx):
