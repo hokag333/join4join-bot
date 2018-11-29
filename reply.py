6e523a9b56f2506e6ed3c3885f24cc142abfe521
Prisa <43382953+ElPrisa@users.noreply.github.com>
2018-11-29 17:39:44 +0100

    Update reply.py

--- reply.py
+++ reply.py
@@ -1,1 +1,18 @@
+import discord
+from discord.ext import commands
 
+class Reply:
+  def __init__(self, bot):
+    self.bot = bot
+    
+  
+  async def on_message(self, message):
+    if message.content.startswith('test me'):
+      await self.bot.send_message(message.channel, 'testing')
+      
+  @commands.command()
+  async def ping(self):
+    await self.bot.say('bang')
+    
+def setup(bot):
+  bot.add_cog(Reply(bot))
