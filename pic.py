55051232ffa6980a53122fde69d7365cd74c90be
Prisa <43382953+ElPrisa@users.noreply.github.com>
2018-11-29 17:42:10 +0100

    Update pic.py

--- pic.py
+++ pic.py
@@ -1,1 +1,18 @@
+import discord
+from discord.ext import commands
 
+class Pic:
+  def __init__(self, bot):
+    self.bot = bot
+    
+    
+  async def on_message(self, message):
+    if message.content.startswith('embed'):
+      await self.bot.send_message(message.channel, 'it is not set')
+      
+  @commands.command()
+  async def testall(self):
+    await self.bot.say('lol')
+    
+def setup(bot):
+  bot.add_cog(Pic(bot))
