fa01eaa424a11a9aad973e10f725a54500cfd163
Prisa <43382953+ElPrisa@users.noreply.github.com>
2018-12-06 19:56:04 +0100

    Update tier.py

--- tier.py
+++ tier.py
@@ -1,1 +1,19 @@
+import discord
+from discord.ext import commands
 
+class Tier:
+  def __init__(self, bot):
+    self.bot = bot
+    
+    
+    async def on_message(self, message):
+      if message.author == self.bot.user:
+        return
+      if message.author.bot: return
+      
+      if message.content.startswith('tiertest'):
+        await self.bot.send_message(message.channel, 'new Class tested')
+        
+        
+def setup(bot):
+  bot.add_cog(Tier(bot))
