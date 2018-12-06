b3ff1e20bfe8a90fa7cd3f9832f8a088b0dcdb3b
Prisa <43382953+ElPrisa@users.noreply.github.com>
2018-12-06 22:22:34 +0100

    Update reply.py

--- reply.py
+++ reply.py
@@ -136,7 +136,7 @@ class Reply:
         await self.bot.send_message(message.channel, ' **{}** \n'
                                     'You have not any Tier'.format(message.author.name))
         
-    if message.content.startswith('testmetiers'):
+    if message.content.startswith('askdjajsda'):
       await self.bot.send_message(message.channel, 'commands is in testing for now if they work fine')
         
       
