a639d228a34fc0bc2aec16f03bdeef043c894ce1
Prisa <43382953+ElPrisa@users.noreply.github.com>
2018-12-07 15:10:00 +0100

    Update tier.py

--- tier.py
+++ tier.py
@@ -11,17 +11,65 @@ class Tier:
       return
     if message.author.bot: return
       
-    if message.content.startswith('none'):
-      await self.bot.send_message(message.channel, 'new Class tested')
+    if message.content.startswith('!Tier'):
+      if '508606486864461824' in (role.id for role in message.author.roles):
+        await self.bot.send_message(message.channel, ' **{}** \n'
+                                    'You have: **Tier X** '.format(message.author.name))
+        return
+      if '508606351493169155' in (role.id for role in message.author.roles):
+        await self.bot.send_message(message.channel, ' **{}** \n'
+                                    'You have: **Tier IX** '.format(message.author.name))
+        return
+      if '508606276666654720' in (role.id for role in message.author.roles):
+        await self.bot.send_message(message.channel, ' **{}** \n'
+                                    'You have: **Tier VIII** '.format(message.author.name))
+        return
+      if '508606197461680138' in (role.id for role in message.author.roles):
+        await self.bot.send_message(message.channel, ' **{}** \n'
+                                    'You have: **Tier VII** '.format(message.author.name))
+        return
+      if '508606126497988609' in (role.id for role in message.author.roles):
+        await self.bot.send_message(message.channel, ' **{}** \n'
+                                    'You have: **Tier VI** '.format(message.author.name))
+        return
+      if '508606074736214036' in (role.id for role in message.author.roles):
+        await self.bot.send_message(message.channel, ' **{}** \n'
+                                    'You have: **Tier V** '.format(message.author.name))
+        return
+      if '508605954875719698' in (role.id for role in message.author.roles):
+        await self.bot.send_message(message.channel, ' **{}** \n'
+                                    'You have: **Tier IV** '.format(message.author.name))
+        return
+      if '508605725874978825' in (role.id for role in message.author.roles):
+        await self.bot.send_message(message.channel, ' **{}** \n'
+                                    'You have: **Tier III** '.format(message.author.name))
+        return
+      if '508605659143471104' in (role.id for role in message.author.roles):
+        await self.bot.send_message(message.channel, ' **{}** \n'
+                                    'You have: **Tier II** '.format(message.author.name))
+        return
+      if '508605450770448384' in (role.id for role in message.author.roles):
+        await self.bot.send_message(message.channel, ' **{}** \n'
+                                    'You have: **Tier I** '.format(message.author.name))
+        return
+      else:
+        await self.bot.send_message(message.channel, ' **{}** \n'
+                                    'You have not any Tier'.format(message.author.name))
       
       
   @commands.command(pass_context=True)
   async def mytier(self, ctx, user: discord.User):
+    if not user:
+      if "508606486864461824" in (role.id for role in message.author.roles):
+        await self.bot.say(" **{}** \n"
+                           "You have: **Tier X** ".format(message.author.name))
+        return
     if "508606486864461824" in (role.id for role in user.roles):
       await self.bot.say(" **{}** \n"
                                   "You have: **Tier X** ".format(user.name))
     else:
-      await self.bot.say(" error")
+      await self.bot.say(" **{}** \n"
+                         "You have not any Tier".format(user.name))
         
         
 def setup(bot):
