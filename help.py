4175b24cdb32201956c97e64d7adddf5d941ad83
Prisa <43382953+ElPrisa@users.noreply.github.com>
2018-12-08 14:17:34 +0100

    Update help.py

--- help.py
+++ help.py
@@ -19,7 +19,26 @@ class Helper:
       embed.set_image(url='')
       embed.set_author(name='', icon_url='')
       embed.add_field(name='.cmds', value= 'all commands of bot', inline=True)
-      embed.add_field(name='.responses', value= 'all chatting cmds to get  bot responses', inline=True)
+      embed.add_field(name='.responses', value= 'all chatting cmds to get bot responses', inline=True)
+      await self.bot.send_message(message.channel, embed=embed)
+      
+    if ('.cmds') in message.content:
+      embed = discord.Embed(title = 'Help', description = 'cmds', colour = discord.Colour.blue())
+      embed.set_footer(text='')
+      embed.set_thumbnail(url='')
+      embed.set_image(url='')
+      embed.set_author(name='', icon_url='')
+      embed.add_field(name='!Tier', value= 'show your **Tier**', inline=False)
+      embed.add_field(name='!Tier @user_mention', value= 'show user´s**Tier**', inline=True)
+      await self.bot.send_message(message.channel, embed=embed)
+      
+    if ('.responses') in message.content:
+      embed = discord.Embed(title = 'Help', description = 'responses', colour = discord.Colour.blue())
+      embed.set_footer(text='')
+      embed.set_thumbnail(url='')
+      embed.set_image(url='')
+      embed.set_author(name='', icon_url='')
+      embed.add_field(name='', value= 'not set', inline=True)
       await self.bot.send_message(message.channel, embed=embed)
       
       
