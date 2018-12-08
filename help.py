950084ae2b369b55ce629afd090d1dcaacaec058
Prisa <43382953+ElPrisa@users.noreply.github.com>
2018-12-08 14:09:17 +0100

    Update help.py

--- help.py
+++ help.py
@@ -13,7 +13,14 @@ class Helper:
     
     
     if ('.help') in message.content:
-      await self.bot.send_message(message.channel, 'helping')
+      embed = discord.Embed(title = 'Help', description = '', colour = discord.Colour.blue())
+      embed.set_footer(text='')
+      embed.set_thumbnail(url='')
+      embed.set_image(url='')
+      embed.set_author(name='', icon_url='')
+      embed.add_field(name='.cmds', value= 'all commands of bot', inline=True)
+      embed.add_field(name='.responses', value= 'all chatting cmds to get  bot responses', inline=True)
+      await self.bot.send_message(message.channel, embed=embed)
       
       
   @commands.command()
