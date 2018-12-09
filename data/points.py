a2660bdb1346ffc1d4a90faa581ee524876f0a20
Prisa <43382953+ElPrisa@users.noreply.github.com>
2018-12-09 20:24:20 +0100

    Update points.py

--- data/points.py
+++ data/points.py
@@ -12,14 +12,33 @@ class Points:
     if message.author.bot: return
     
     if message.content.startswith('!points help'):
-      await self.bot.send_message(message.channel, ' **{}**  to view your points write !points \n'.format(message.author.name))
+      await self.bot.send_message(message.channel, ' **{}**  to view your points write !points'.format(message.author.name))
+      
+    if message.content.startswith('!points register'):
+      await self.bot.send_message(520911898909212676, ' **{}**  was registered to points board'.format(message.author.name))
       
   @commands.command(pass_context=True)
   async def points(self, ctx, user: discord.User=None):
     if not user:
-      #Prisa#4835
+      #4835
       if "381887710308335618" in (ctx.message.author.id):
         await self.bot.say(" **{}** you have **∞** points".format(ctx.message.author.name))
+        return
+      #7553
+      if "461967586058305537" in (ctx.message.author.id):
+        await self.bot.say(" **{}** you have **5 500** points".format(ctx.message.author.name))
+        return
+      #1056
+      if "466673021248208896" in (ctx.message.author.id):
+        await self.bot.say(" **{}** you have **1 000** points".format(ctx.message.author.name))
+        return
+      #3046
+      if "310264325497421824" in (ctx.message.author.id):
+        await self.bot.say(" **{}** you have **1 000** points".format(ctx.message.author.name))
+        return
+      #6739
+      if "373867168280084496" in (ctx.message.author.id):
+        await self.bot.say(" **{}** you have **500** points".format(ctx.message.author.name))
         return
       else:
         await self.bot.say(" **{}** you don´t have any points \n"
