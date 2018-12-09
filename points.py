347479af94ceb1c0ffe37f70b0faec176e8c87a9
Prisa <43382953+ElPrisa@users.noreply.github.com>
2018-12-09 18:55:55 +0100

    Update points.py

--- points.py
+++ points.py
@@ -1,1 +1,29 @@
+import discord
+from discord.ext import commands
 
+class Points:
+  def __init__(self, bot):
+    self.bot = bot
+    
+    
+  async def on_message(self, message):
+    if message.author == self.bot.user:
+      return
+    if message.author.bot: return
+    
+    if message.content.startswith('trynewpoints'):
+      await self.bot.send_message(message.channel, 'points comming soon')
+      
+      
+  @commands.command(pass_context=True)
+  async def give(self, ctx, member: discord.Member):
+    points[member.id] += 1
+    self.bot.say("{} now has {} points".format(member.mention, points[member.id]))
+    
+  @commands.command(pass_context=True)
+  async def points(self, ctx, member: discord.Member):
+    self.bot.say("{} has {} points".format(member.mention, points[member.id]))
+    
+
+def setup(bot):
+  bot.add_cog(Points(bot))
