f870a251ed4997e82abeb954d1c695d37d8f9b76
Prisa <43382953+ElPrisa@users.noreply.github.com>
2018-12-18 18:22:28 +0100

    Update userinfo.py

--- data/userinfo.py
+++ data/userinfo.py
@@ -14,7 +14,8 @@ class Userinfo:
     embed=discord.Embed(title=" ", description="{}".format(ctx.message.author.name), color=0xfed83d)
     embed.set_author(name=" ")
     embed.set_image(url=ctx.message.author.avatar_url)
-    embed.add_field(name="test", value="test", inline=True)
+    embed.add_field(name="Higgest role", value="{}".format(ctx.message.author.top_role.mention, inline=True)
+    embed.add_field(name="Joined", value="{}".format(ctx.message.author.joined_at.date, inline=True)
     await self.bot.send_message(ctx.message.channel, embed=embed)
     
     
