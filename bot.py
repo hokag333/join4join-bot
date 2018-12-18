cc0f777dfd6862ddfc1fc5c608db59b63f028bf9
Prisa <43382953+ElPrisa@users.noreply.github.com>
2018-12-18 18:34:13 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,9 +11,16 @@ bot.remove_command('help')
 
 extensions = ['reply', 'pic', 'data.database.tier', 'help', 'data.points', 'data.userinfo']
 
+async def my_background_task():
+    await bot.wait_until_ready()
+    while not bot.is_closed:
+        await bot.change_presence(game=discord.Game(name='Chicken Server'))
+        await asyncio.sleep(5)
+        await bot.change_presence(game=discord.Game(name='fixing'))
+        await asyncio.sleep(5)
+        
 @bot.event
 async def on_ready():
-  await bot.change_presence(game=discord.Game(name='Chicken Server'))
   print('Bot is ready.')
   
 if __name__ == '__main__':
@@ -23,4 +30,5 @@ if __name__ == '__main__':
     except Exception as error:
       print('{} cannot be loaded. [{}]'.format(extension, error))
 
+bot.loop.create_task(my_background_task())
 bot.run(os.environ['BOT_TOKEN'])
