cb14887d4c7b1491e4280c105c47d136f0faea27
Prisa <43382953+ElPrisa@users.noreply.github.com>
2018-12-19 13:51:36 +0100

    Update userinfo.py

--- data/userinfo.py
+++ data/userinfo.py
@@ -11,9 +11,9 @@ class Userinfo:
     if not user:
       user = ctx.message.author
     if "520630368668483594" in(role.id for role in user.roles):
-      tierid = "Tier I"
+      tierid = "<@&520630368668483594>" 
     else:
-      tierid = "zero test" 
+      tierid = "none" 
     embed=discord.Embed(title="User", description="{}".format(user.name), color=0xfed83d)
     embed.set_author(name=" ")
     embed.set_image(url=user.avatar_url)
