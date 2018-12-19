abc7d3e118680521f33019c25639d330270c1cc2
Prisa <43382953+ElPrisa@users.noreply.github.com>
2018-12-19 08:16:24 +0100

    Update userinfo.py

--- data/userinfo.py
+++ data/userinfo.py
@@ -11,8 +11,6 @@ class Userinfo:
     embed=discord.Embed(title=" ", description="{}".format(ctx.message.author.name), color=0xfed83d)
     embed.set_author(name=" ")
     embed.set_image(url=ctx.message.author.avatar_url)
-    embed.add_field(name="Account Created", value=ctx.message.author.created_at.__format__('%A, %d. %B %Y @ %H:%M:%S')), inline=True)
-    embed.add_field(name="Joined at", value=ctx.message.author.joined_at.__format__('%A, %d. %B %Y @ %H:%M:%S')), inline=True)
     await self.bot.send_message(ctx.message.channel, embed=embed)
     
     
