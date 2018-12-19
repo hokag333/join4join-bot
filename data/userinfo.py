cd0cfc68211e41587aec89d40926af3d1b0ce270
Prisa <43382953+ElPrisa@users.noreply.github.com>
2018-12-19 09:44:15 +0100

    Update userinfo.py

--- data/userinfo.py
+++ data/userinfo.py
@@ -11,8 +11,7 @@ class Userinfo:
     embed=discord.Embed(title=" ", description="{}".format(ctx.message.author.name), color=0xfed83d)
     embed.set_author(name=" ")
     embed.set_image(url=ctx.message.author.avatar_url)
-    if "Tier I" in(role.name for roles in ctx.message.author.role):
-      embed.add_field(name="Tier", value="Tier I", inline=True)
+    embed.add_field(name="top role", value="{}".format(ctx.mesage.author.top_role) 
     await self.bot.send_message(ctx.message.channel, embed=embed)
     
     
