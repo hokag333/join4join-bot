4ebac8f772bafecb0877ab7a7748846ea38b5e37
Prisa <43382953+ElPrisa@users.noreply.github.com>
2018-12-19 11:12:09 +0100

    Update userinfo.py

--- data/userinfo.py
+++ data/userinfo.py
@@ -8,12 +8,16 @@ class Userinfo:
     
   @commands.command(pass_context=True)
   async def userinfo(self, ctx, user: discord.User=None):
+    rolelist = {
+     } 
     if not user:
       user = ctx.message.author
     embed=discord.Embed(title=" ", description="{}".format(user.name), color=0xfed83d)
     embed.set_author(name=" ")
     embed.set_image(url=user.avatar_url)
-    embed.add_field(name="Highest role", value=" {} ".format(user.top_role.mention), inline=True) 
+    embed.add_field(name="Highest role", value=" {} ".format(user.top_role.mention), inline=True)
+    if rolelist in(role.id for roles in user.role):
+      embed.add_filed(neme="Tier", value="{}".format(role.mention for roles in rolelist), inline=True) 
     await self.bot.send_message(ctx.message.channel, embed=embed)
     
     
