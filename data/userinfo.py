59ea32052159e5c995967aba03bba9fdb4e1e934
Prisa <43382953+ElPrisa@users.noreply.github.com>
2018-12-19 09:19:12 +0100

    Update userinfo.py

--- data/userinfo.py
+++ data/userinfo.py
@@ -11,6 +11,8 @@ class Userinfo:
     embed=discord.Embed(title=" ", description="{}".format(ctx.message.author.name), color=0xfed83d)
     embed.set_author(name=" ")
     embed.set_image(url=ctx.message.author.avatar_url)
+    if role in(ctx.message.author.roles):
+      embed.add_field(name="role", value="{} ".format(role.name), inline=True)
     await self.bot.send_message(ctx.message.channel, embed=embed)
     
     
