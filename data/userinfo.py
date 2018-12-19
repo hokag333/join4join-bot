dddf036dbc988d06387cbc2cfe2667e205d68ffc
Prisa <43382953+ElPrisa@users.noreply.github.com>
2018-12-19 11:49:09 +0100

    Update userinfo.py

--- data/userinfo.py
+++ data/userinfo.py
@@ -14,8 +14,7 @@ class Userinfo:
     embed.set_author(name=" ")
     embed.set_image(url=user.avatar_url)
     embed.add_field(name="Highest role", value=" {} ".format(user.top_role.mention), inline=True)
-    if "508605450770448384" in(role.id for roles in user.role):
-      await embed.add_filed(neme="Tier", value=" <@&508605450770448384> test", inline=True) 
+    embed.add_filed(neme="Tier", value=" <@&508605450770448384> test", inline=True) 
     await self.bot.send_message(ctx.message.channel, embed=embed)
     
     
