ff669bba5e4e747c91a60a12cf3227394e52debb
Prisa <43382953+ElPrisa@users.noreply.github.com>
2018-12-19 19:41:15 +0100

    Update userinfo.py

--- data/userinfo.py
+++ data/userinfo.py
@@ -110,6 +110,7 @@ class Userinfo:
     embed.add_field(name="Highest role", value=" {} ".format(user.top_role.mention), inline=True)
     embed.add_field(name="Tier", value=" {} ".format(tierid), inline=True)
     embed.add_field(name="Joined at", value=" {} ".format(user.joined_at.strftime(" %d %B %Y ")), inline=False)
+    embed.add_field(name="Invites", value=" {} ".format(user.invites), inline=False)
     await self.bot.send_message(ctx.message.channel, embed=embed)
     
     
