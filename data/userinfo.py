9e4ce07b2d1262aedbf79726291d813c029ea4f7
Prisa <43382953+ElPrisa@users.noreply.github.com>
2018-12-19 10:36:03 +0100

    Update userinfo.py

--- data/userinfo.py
+++ data/userinfo.py
@@ -10,9 +10,9 @@ class Userinfo:
   async def me(self, ctx, user: discord.User=None):
     if not user:
       user = ctx.message.author
-    embed=discord.Embed(title=" ", description="{}".format(ctx.message.author.name), color=0xfed83d)
+    embed=discord.Embed(title=" ", description="{}".format(user.name), color=0xfed83d)
     embed.set_author(name=" ")
-    embed.set_image(url=ctx.message.author.avatar_url)
+    embed.set_image(url=user.avatar_url)
     embed.add_field(name="Highest role", value=" {} ".format(user.top_role.mention), inline=True) 
     await self.bot.send_message(ctx.message.channel, embed=embed)
     
