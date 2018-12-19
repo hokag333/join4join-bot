f440e4105a688221220d33a8e6e32b11fbc18c6b
Prisa <43382953+ElPrisa@users.noreply.github.com>
2018-12-19 19:15:43 +0100

    Update userinfo.py

--- data/userinfo.py
+++ data/userinfo.py
@@ -10,104 +10,100 @@ class Userinfo:
   async def userinfo(self, ctx, user: discord.User=None):
     if not user:
       user = ctx.message.author
-    try:
-      #Diamond Tiers
-      if "520631199710969864" in(role.id for role in user.roles):
-        tierid = "<@&520631199710969864>"
-        return infotest
+    #Diamond Tiers
+    if "520631199710969864" in(role.id for role in user.roles):
+      tierid = "<@&520631199710969864>"
+    else:
       if "" in(role.id for role in user.roles):
         tierid = "<@&520631201778892810>"
-        return
-      if "520631013748113418" in(role.id for role in user.roles):
-        tierid = "<@&520631013748113418>"
-        return
-      if "520631017376186388" in(role.id for role in user.roles):
-        tierid = "<@&520631017376186388>"
-        return
-      if "520631021125763082" in(role.id for role in user.roles):
-        tierid = "<@&520631021125763082>"
-        return
-      if "520630751897583626" in(role.id for role in user.roles):
-        tierid = "<@&520630751897583626>"
-        return
-      if "520630661678235648" in(role.id for role in user.roles):
-        tierid = "<@&520630661678235648>"
-        return
-      if "520630586369507350" in(role.id for role in user.roles):
-        tierid = "<@&520630586369507350>"
-        return
-      if "520630502827229184" in(role.id for role in user.roles):
-        tierid = "<@&520630502827229184>"
-        return
-      if "520630368668483594" in(role.id for role in user.roles):
-        tierid = "<@&520630368668483594>"
-        return
-      #Platinum Tiers
-      if "520632449898774550" in(role.id for role in user.roles):
-        tierid = "<@&520632449898774550>"
-        return
-      if "520632455305232384" in(role.id for role in user.roles):
-        tierid = "<@&520632455305232384>"
-        return
-      if "520632457930997770" in(role.id for role in user.roles):
-        tierid = "<@&520632457930997770>"
-        return
-      if "520632460422283290" in(role.id for role in user.roles):
-        tierid = "<@&520632460422283290>"
-        return
-      if "520632462351663109" in(role.id for role in user.roles):
-        tierid = "<@&520632462351663109>"
-        return
-      if "520632464323117056" in(role.id for role in user.roles):
-        tierid = "<@&520632464323117056>"
-        return
-      if "520632466311086090" in(role.id for role in user.roles):
-        tierid = "<@&520632466311086090>"
-        return
-      if "520632468328415233" in(role.id for role in user.roles):
-        tierid = "<@&520632468328415233>"
-        return
-      if "520632470253862912" in(role.id for role in user.roles):
-        tierid = "<@&520632470253862912>"
-        return
-      if "520632473583878164" in(role.id for role in user.roles):
-        tierid = "<@&520632473583878164>"
-        return
-      #Normal Tiers
-      if "508606486864461824" in(role.id for role in user.roles):
-        tierid = "<@&508606486864461824>"
-        return
-      if "508606351493169155" in(role.id for role in user.roles):
-        tierid = "<@&508606351493169155>"
-        return
-      if "508606276666654720" in(role.id for role in user.roles):
-        tierid = "<@&508606276666654720>"
-        return
-      if "508606197461680138" in(role.id for role in user.roles):
-        tierid = "<@&508606197461680138>"
-        return
-      if "508606126497988609" in(role.id for role in user.roles):
-        tierid = "<@&508606126497988609>"
-        return
-      if "508606074736214036" in(role.id for role in user.roles):
-        tierid = "<@&508606074736214036>"
-        return
-      if "508605954875719698" in(role.id for role in user.roles):
-        tierid = "<@&508605954875719698>"
-        return
-      if "508605725874978825" in(role.id for role in user.roles):
-        tierid = "<@&508605725874978825>"
-        return
-      if "508605659143471104" in(role.id for role in user.roles):
-        tierid = "<@&508605659143471104>"
-        return
-      if "508605450770448384" in(role.id for role in user.roles):
-        tierid = "<@&508605450770448384>"
-        return
       else:
-        tierid = "none"
-        return
-      
+        if "520631013748113418" in(role.id for role in user.roles):
+          tierid = "<@&520631013748113418>"
+        else:
+          if "520631017376186388" in(role.id for role in user.roles):
+            tierid = "<@&520631017376186388>"
+          else:
+            if "520631021125763082" in(role.id for role in user.roles):
+              tierid = "<@&520631021125763082>"
+            else:
+              if "520630751897583626" in(role.id for role in user.roles):
+                tierid = "<@&520630751897583626>"
+              else:
+                if "520630661678235648" in(role.id for role in user.roles):
+                  tierid = "<@&520630661678235648>"
+                else:
+                  if "520630586369507350" in(role.id for role in user.roles):
+                    tierid = "<@&520630586369507350>"
+                  else:
+                    if "520630502827229184" in(role.id for role in user.roles):
+                      tierid = "<@&520630502827229184>"
+                    else:
+                      if "520630368668483594" in(role.id for role in user.roles):
+                        tierid = "<@&520630368668483594>"
+                      else:
+                        #Platinum Tiers
+                        if "520632449898774550" in(role.id for role in user.roles):
+                          tierid = "<@&520632449898774550>"
+                        else:
+                          if "520632455305232384" in(role.id for role in user.roles):
+                            tierid = "<@&520632455305232384>"
+                          else:
+                            if "520632457930997770" in(role.id for role in user.roles):
+                              tierid = "<@&520632457930997770>"
+                            else:
+                              if "520632460422283290" in(role.id for role in user.roles):
+                                tierid = "<@&520632460422283290>"
+                              else:
+                                if "520632462351663109" in(role.id for role in user.roles):
+                                  tierid = "<@&520632462351663109>"
+                                else:
+                                  if "520632464323117056" in(role.id for role in user.roles):
+                                    tierid = "<@&520632464323117056>"
+                                  else:
+                                    if "520632466311086090" in(role.id for role in user.roles):
+                                      tierid = "<@&520632466311086090>"
+                                    else:
+                                      if "520632468328415233" in(role.id for role in user.roles):
+                                        tierid = "<@&520632468328415233>"
+                                      else:
+                                        if "520632470253862912" in(role.id for role in user.roles):
+                                          tierid = "<@&520632470253862912>"
+                                        else:
+                                          if "520632473583878164" in(role.id for role in user.roles):
+                                            tierid = "<@&520632473583878164>"
+                                          else:
+                                            #Normal Tiers
+                                            if "508606486864461824" in(role.id for role in user.roles):
+                                              tierid = "<@&508606486864461824>"
+                                            else:
+                                              if "508606351493169155" in(role.id for role in user.roles):
+                                                tierid = "<@&508606351493169155>"
+                                              else:
+                                                if "508606276666654720" in(role.id for role in user.roles):
+                                                  tierid = "<@&508606276666654720>"
+                                                else:
+                                                  if "508606197461680138" in(role.id for role in user.roles):
+                                                    tierid = "<@&508606197461680138>"
+                                                  else:
+                                                    if "508606126497988609" in(role.id for role in user.roles):
+                                                      tierid = "<@&508606126497988609>"
+                                                    else:
+                                                      if "508606074736214036" in(role.id for role in user.roles):
+                                                        tierid = "<@&508606074736214036>"
+                                                      else:
+                                                        if "508605954875719698" in(role.id for role in user.roles):
+                                                          tierid = "<@&508605954875719698>"
+                                                        else:
+                                                          if "508605725874978825" in(role.id for role in user.roles):
+                                                            tierid = "<@&508605725874978825>"
+                                                          else:
+                                                            if "508605659143471104" in(role.id for role in user.roles):
+                                                              tierid = "<@&508605659143471104>"
+                                                            else:
+                                                              if "508605450770448384" in(role.id for role in user.roles):
+                                                                tierid = "<@&508605450770448384>"
+                                                              else:
+                                                                tierid = "none"
     embed=discord.Embed(title="User", description="{}".format(user.name), color=0xfed83d)
     embed.set_author(name=" ")
     embed.set_image(url=user.avatar_url)
