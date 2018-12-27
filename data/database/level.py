6ef6f51a47f22f452fe8b7d2c719bc223a80e22e
Prisa <43382953+ElPrisa@users.noreply.github.com>
2018-12-27 15:49:57 +0100

    Update level.py

--- data/database/level.py
+++ data/database/level.py
@@ -1,2 +1,12 @@
 import discord
 from discord.ext import commands
+
+class Level:
+  def __init__(self, bot):
+    self.bot = bot
+    
+  @commands.command(pass_context=True)
+  async def pf(self, ctx, user: discord.User=None):
+    
+def setup(bot):
+  bot.add_cog(Level(bot))
