3b2dfba4f1d64ebcca340daf81793d6aed19b5f0
Prisa <43382953+ElPrisa@users.noreply.github.com>
2018-12-28 15:49:09 +0100

    Update commands.py

--- data/commands.py
+++ data/commands.py
@@ -12,7 +12,7 @@ class Commands:
   @commands.command(pass_context=True)
   async def verify(self, ctx):
     await self.bot.delete_message(ctx.message)
-    await self.bot.send_message(ctx.message.channel, " {} waiting for verification".format(ctx.message.author.mention))
+    await self.bot.send_message(ctx.message.channel, " {} waiting for verification🌀".format(ctx.message.author.mention))
     
   @commands.command(pass_context=True)
   async def verified(self, ctx, user: discord.User):
