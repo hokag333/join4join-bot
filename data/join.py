7bafc28c080fb5eecb02e7929970fb3fe12a33db
Prisa <43382953+ElPrisa@users.noreply.github.com>
2018-12-28 19:17:10 +0100

    Update join.py

--- data/join.py
+++ data/join.py
@@ -1,1 +1,15 @@
+import discord
+from discord.ext import commands
 
+class Join:
+  def __init__(self, bot):
+    self.bot = bot 
+                                
+  async def on_member_join(self, member):
+    await self.bot.send_message(member, "Hello {} \n"
+                                "If you want to have permissions to post invite links on **Join 4 Join** server \n"
+                                "go to the <#528209980734832642> and write **.verify** to verification your account".format(member.mention)
+    
+    
+def setup(bot):
+  bot.add_cog(Join(bot))
