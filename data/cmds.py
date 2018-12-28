e00dd1037e36c8b46e02414b45cefdb027b91c9a
Prisa <43382953+ElPrisa@users.noreply.github.com>
2018-12-28 12:08:25 +0100

    Update cmds.py

--- data/cmds.py
+++ data/cmds.py
@@ -1,1 +1,18 @@
+import discord
+from discord.ext import commands
 
+class Cmds:
+  def __init__(self, bot):
+    self.bot = bot
+    
+    
+  @commands.command(pass_context=True)
+  async def dm(self, ctx, reason):
+    await self.bot.delete_message(ctx.message)
+    await self.bot.send_message(ctx.message.channel," {} \n"
+                                "by user {} ".format(reason, ctx.message.author.mention)
+
+    
+    
+def setup(bot):
+  bot.add_cog(Cmds(bot))
