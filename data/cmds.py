6488bb631debcb4e910453d492894f0f452a1952
Prisa <43382953+ElPrisa@users.noreply.github.com>
2018-12-28 12:34:09 +0100

    Update cmds.py

--- data/cmds.py
+++ data/cmds.py
@@ -7,6 +7,15 @@ class Cmds:
     
     
   @commands.command(pass_context=True)
+  async def clear(self, ctx, amount=100):
+    channel = ctx.message.channel
+    messages = []
+    async for message in self.bot.logs_from(channel, limit=int(amount) + 1):
+      messages.append(message)
+    await self.bot.delete_messages(messages)
+    await self.bot.say("messages deleted")
+    
+  @commands.command(pass_context=True)
   async def dm(self, ctx):
     await self.bot.send_message(ctx.message.channel," test dm \n"
                                 "by user {} ".format(ctx.message.author.mention)
