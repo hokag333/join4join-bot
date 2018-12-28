fad051ae3522a6c7f08dc7ef374fbea6454f7814
Prisa <43382953+ElPrisa@users.noreply.github.com>
2018-12-28 23:47:36 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -18,7 +18,7 @@ async def my_background_task():
         await asyncio.sleep(5)
         await bot.change_presence(game=discord.Game(name='version 1.2.1'))
         await asyncio.sleep(5)
-        await bot.change_presence(game=discord.Game(name='12 members'))
+        await bot.change_presence(game=discord.Game(name='{} members'.format(server.members)))
         await asyncio.sleep(5)
         
 @bot.event
