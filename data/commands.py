6b0b48052d596f0028f49b77097fcf00655262d0
Prisa <43382953+ElPrisa@users.noreply.github.com>
2018-12-28 15:01:35 +0100

    Update commands.py

--- data/commands.py
+++ data/commands.py
@@ -6,8 +6,13 @@ class Commands:
     self.bot = bot
     
   @commands.command(pass_context=True)
-  async def dm(self, ctx,):
+  async def dm(self, ctx):
     await self.bot.say(" test message by {}".format(ctx.message.author.mention))
+    
+  @commands.command(pass_context=True)
+  async def verify(self, ctx,):
+    await self.bot.delete_message(ctx.message)
+    await self.bot.send_message(ctx.message.channel, "test verification")
     
 def setup(bot):
   bot.add_cog(Commands(bot))
