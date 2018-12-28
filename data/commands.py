56303d6d453d73a2812f1f1aab1b037f4268107d
Prisa <43382953+ElPrisa@users.noreply.github.com>
2018-12-28 14:24:51 +0100

    Update commands.py

--- data/commands.py
+++ data/commands.py
@@ -5,25 +5,6 @@ class Commands:
   def __init__(self, bot):
     self.bot = bot
     
-  async def on_message(self, message):
-    if message.content.startswith('test'):
-      await self.bot.send_message(message.channel, 'test')
-      
-  @commands.command(pass_context=True)
-  async def clear(self, ctx, amount=100):
-    channel = ctx.message.channel
-    messages = []
-    async for message in self.bot.logs_from(channel, limit=int(amount) + 1):
-      messages.append(message)
-    await self.bot.delete_messages(messages)
-    await self.bot.say("messages deleted")
-    
-  @commands.command(pass_context=True)
-  async def dm(self, ctx, *, reason):
-    await self.bot.send_message(ctx.message.channel," \n" + reason, 
-                                "by user {} ".format(ctx.message.author.mention)
-    await self.bot.send_message(server_member, " "  + reason)
-
     
     
 def setup(bot):
