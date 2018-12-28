ddce95cfc08a3859ba3cc7aa7fe81000dcb57a0b
Prisa <43382953+ElPrisa@users.noreply.github.com>
2018-12-28 14:38:02 +0100

    Update commands.py

--- data/commands.py
+++ data/commands.py
@@ -7,8 +7,7 @@ class Commands:
     
   @commands.command(pass_context=True)
   async def dm(self, ctx):
-    await self.bot.say("test \n"
-                       " {}".format(ctx.message.author.mention)
+    await self.bot.say(" {}".format(ctx.message.author.mention)
     
 def setup(bot):
   bot.add_cog(Commands(bot))
