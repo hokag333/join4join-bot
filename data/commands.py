d7a503d400badd03a34cd3af56e046b221a131cd
Prisa <43382953+ElPrisa@users.noreply.github.com>
2018-12-28 20:11:33 +0100

    Update commands.py

--- data/commands.py
+++ data/commands.py
@@ -39,7 +39,7 @@ class Commands:
                                 "💬**Chat Rooms** and 🔈**Voice Rooms** \n"
                                 "and more \n"
                                 " \n"
-                                "**Invite** : {}**__to verification join this server__** \n"
+                                "**Invite** : {} **__to verification join this server__** \n"
                                 "https://discord.gg/9hjAVpP".format(ctx.message.author.mention))
     await self.bot.send_message(ctx.message.channel, " {} waiting for verification🌀".format(ctx.message.author.mention))
     
