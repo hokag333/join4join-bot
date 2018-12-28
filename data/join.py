7c70350ebaa542a1494754c0237a187cdf14d1ec
Prisa <43382953+ElPrisa@users.noreply.github.com>
2018-12-28 22:27:17 +0100

    Update join.py

--- data/join.py
+++ data/join.py
@@ -7,8 +7,12 @@ class Join:
                                 
   async def on_member_join(self, member):
     await self.bot.send_message(member, "Hello {} \n"
-                                "If you want to have permissions to post invite links in **Join 4 Join** server \n"
-                                "go to the <#528209980734832642> and write **.verify** to verification your account".format(member.mention)
+                                "👋Welcome👋 to **Join 4 Join** server \n"
+                                " \n"
+                                "write **.verify** in <#528209980734832642> to verification✅ your account \n"
+                                "check <#528148347698020353> and read 📝information📝 how to use **Join 4 Join** server and bot
+                                "If you need help with something write **.help** in <#528147248706486282> \n"
+                                "or contact <@&528200802863677450>".format(member.mention)
     
     
 def setup(bot):
