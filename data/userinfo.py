0ed21103ec2ea31e50d361607c50f95a55ddf401
Prisa <43382953+ElPrisa@users.noreply.github.com>
2018-12-29 17:56:39 +0100

    Update userinfo.py

--- data/userinfo.py
+++ data/userinfo.py
@@ -18,6 +18,11 @@ class Userinfo:
     embed.add_field(name="Account Created at", value=" {} ".format(user.created_at.strftime(" %d %B %Y ")), inline=False)
     await self.bot.send_message(ctx.message.channel, embed=embed)
     
+  @commands.command(pass_context=True)
+  async def testme(self, ctx):
+    if "458341394524798976" in(server.id for server in ctx.message.author.server):
+      await self.bot.send_message(ctx.message.channel, "{} test complete".format(ctx.message.author.mention))
+    
     
 def setup(bot):
   bot.add_cog(Userinfo(bot))
