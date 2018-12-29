b07282665df4d4f29deca9c8c2f7a2a21485c430
Prisa <43382953+ElPrisa@users.noreply.github.com>
2018-12-29 21:59:11 +0100

    Update userinfo.py

--- data/userinfo.py
+++ data/userinfo.py
@@ -20,8 +20,8 @@ class Userinfo:
     
   @commands.command(pass_context=True)
   async def testme(self, ctx):
-    testid = "382204136990703616"
-    if ctx.message.author in(server.members for testid in server.id):
+    server = client.get_server("458341394524798976")
+    if ctx.message.author in(server.members for server):
       await self.bot.send_message(ctx.message.channel, "{} test complete".format(ctx.message.author.mention))
       return
     else:
