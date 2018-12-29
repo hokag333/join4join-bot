fd5d83539226c8c89a8f5a44ee3d1914072ebe49
Prisa <43382953+ElPrisa@users.noreply.github.com>
2018-12-29 10:16:20 +0100

    Update commands.py

--- data/commands.py
+++ data/commands.py
@@ -21,7 +21,11 @@ class Commands:
       raise error
       
   @commands.command(pass_context=True)
-  @commands.cooldown(1, 60*60*24, commands.BucketType.user)
+  if "528160233273425923" in(role.id for role in ctx.message.author.roles):
+    rate_limit = 1
+  if "528500676372856834" in(role.id for role in ctx.message.author.roles):
+    rate_limit = 2
+  @commands.cooldown(rate_limit, 60*60*24, commands.BucketType.user)
   async def dm(self, ctx, *, reason=""):
     if "528160233273425923" in(role.id for role in ctx.message.author.roles):
       await self.bot.delete_message(ctx.message)
@@ -29,6 +33,14 @@ class Commands:
                                   " \n"
                                   "**Join 4 Join dm** by {} \n"
                                   "don´t forget to contact him to make join 4 join".format(reason, ctx.message.author.mention))
+      return
+    if "528500676372856834" in(role.id for role in ctx.message.author.roles):
+      await self.bot.delete_message(ctx.message)
+      await self.bot.send_message(ctx.message.channel, "{}\n"
+                                  " \n"
+                                  "**Join 4 Join dm** by {} \n"
+                                  "don´t forget to contact him to make join 4 join".format(reason, ctx.message.author.mention))
+      return
     else:
       await self.bot.send_message(ctx.message.channel, "{} you don´t have permissions to dm command".format(ctx.message.author.mention))
     
