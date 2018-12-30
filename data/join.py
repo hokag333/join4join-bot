0b0bf0f7cdc6178e6cd2e89bb8a9fbbe855da74f
Prisa <43382953+ElPrisa@users.noreply.github.com>
2018-12-30 15:34:00 +0100

    Update join.py

--- data/join.py
+++ data/join.py
@@ -5,35 +5,29 @@ class Join:
   def __init__(self, bot):
     self.bot = bot 
                                 
-  async def on_member_join(self, member):
+  async def on_member_join(self, member, server: discord.Server):
+    if member_join(server.id("528142547894272010")):
+      await self.bot.send_message(member, "test message")
+      return
     server1 = self.bot.get_server("528142547894272010")
-    if member.join in(server1):
-      await self.bot.send_message(member, "{} \n"
-                                  "👋Welcome👋 to \n"
-                                  "**    ** **Join 4 Join** server \n"
-                                  " \n"
-                                  "write **.verify** in <#528209980734832642> to verification✅ your account \n"
-                                  "check <#528148347698020353> and read 📝information how to use **Join 4 Join** server and bot \n"
-                                  " \n"
-                                  "If you need help with something, write **.help** in <#528147248706486282> \n"
-                                  "or contact <@&528200802863677450>".format(member.mention))
-      return
-    server2 = self.bot.get_server("382204136990703616")
-    if member.join in(server2):
-      server1 = self.bot.get_server("528142547894272010")
-      if member in(server1.members):
-        await self.bot.send_message(member, "{} Your account was verificate✅".format(member.mention))
+    if member in(server1):
+      server2 = self.bot.get_server("382204136990703616")
+      if member in(server2.members):
+        await self.bot.send_message(member, "{} \n"
+                                    " Your account was verificate✅".format(member.mention))
         return
-    else:
-      return
-    
-  async def on_server_remove(self, server, member):
-    if server.id("528142547894272010"):
-      return
-    if server.id("382204136990703616"):
       server1 = self.bot.get_server("528142547894272010")
-      if member in(server1.members):
-        await self.bot.send_message(member, "test")
+      if member in(server1):
+        await self.bot.send_message(member, "{} \n"
+                                    "👋Welcome👋 to \n"
+                                    "**    ** **Join 4 Join** server \n"
+                                    " \n"
+                                    "write **.verify** in <#528209980734832642> to verification✅ your account \n"
+                                    "check <#528148347698020353> and read 📝information how to use **Join 4 Join** server and bot \n"
+                                    " \n"
+                                    "If you need help with something, write **.help** in <#528147248706486282> \n"
+                                    "or contact <@&528200802863677450>".format(member.mention))
+
     
   async def on_member_remove(self, member):
     server2 = self.bot.get_server("528142547894272010")
