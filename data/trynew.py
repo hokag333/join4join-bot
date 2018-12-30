8ed1b1f7ba617b42d964dbe99898a3b503378d06
Prisa <43382953+ElPrisa@users.noreply.github.com>
2018-12-30 23:10:32 +0100

    Update trynew.py

--- data/trynew.py
+++ data/trynew.py
@@ -24,6 +24,7 @@ class Trynew:
         role = discord.utils.get(server.roles, id="528162784022626314")
         new_member = server.get_member(member.id)
         await self.bot.add_roles(new_member, role)
+        await self.bot.send_message(discord.Object(id='528209980734832642'), "{} was verificate✅".format(member.mention))
  
     
 def setup(bot):
