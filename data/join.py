f2c806d7583aea65cbef54719dfde52384e0fc85
Prisa <43382953+ElPrisa@users.noreply.github.com>
2018-12-30 18:48:36 +0100

    Update join.py

--- data/join.py
+++ data/join.py
@@ -7,6 +7,7 @@ class Join:
                                 
   async def on_member_join(self, member):
     if member.server.id == "528142547894272010":
+      role = self.bot.get_server.role("528200802863677450")
       await self.bot.send_message(member, "👋Welcome👋 \n"
                                   "{} \n"
                                   "to **Join 4 Join** server \n"
@@ -15,7 +16,7 @@ class Join:
                                   "read **information**📝 how to use **Join 4 Join** server and bot in <#528148347698020353>  \n"
                                   " \n"
                                   "If you need help with something, write **.help** \n"
-                                  "or contact <@&528200802863677450>".format(member.mention))
+                                  "or contact {} ".format(member.mention, role.mention))
     elif member.server.id == "458341394524798976":
       server1 = self.bot.get_server("528142547894272010")
       if member in(server1.members):
