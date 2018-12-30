2f40b6aee2e8dc5f29d3d9de182497722e516c44
Prisa <43382953+ElPrisa@users.noreply.github.com>
2018-12-30 19:22:34 +0100

    Update verify.py

--- data/verify.py
+++ data/verify.py
@@ -33,12 +33,11 @@ class Verify:
       if "528145435022327810" in(role.id for role in ctx.message.author.roles):
         await self.bot.add_roles(member, get_role(self.bot.get_server("528142547894272010").roles.id('528162784022626314')
         await self.bot.send_message(user, "{} \n"
-                                    "Your account was verificate✅"
-                                    "**invite links** in **Join 4 Join** server is enabled\n"
-                                    " \n"format(user.mention))
+                                    "Your account was verificate✅ \n"
+                                    "**invite links** in **Join 4 Join** server is enabled ".format(user.mention))
         await self.bot.send_message(self.bot.get_channel('528209980734832642'), "{} was verificate✅".format(user.mention))
         return
-      await self.bot.send_message(ctx.message.channel, "{} you don´t heve permissions to do that".format(ctx.message.author.mention))
+      await self.bot.delete_message(ctx.message)
     
   @commands.command(pass_context=True)
   async def verifyerror(self, ctx, user: discord.User):
@@ -52,6 +51,7 @@ class Verify:
                                     "https://discord.gg/9hjAVpP".format(user.mention))
         await self.bot.send_message(ctx.message.channel, "{} get verification error message".format(user.mention))
         return
+      await self.bot.delete_message(ctx.message)
     
 def setup(bot):
   bot.add_cog(Verify(bot))
