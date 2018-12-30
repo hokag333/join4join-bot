c2d4ac6c56eb00b1c5572971b8db75ea1d37b514
Prisa <43382953+ElPrisa@users.noreply.github.com>
2018-12-30 09:18:29 +0100

    Update join.py

--- data/join.py
+++ data/join.py
@@ -6,15 +6,24 @@ class Join:
     self.bot = bot 
                                 
   async def on_member_join(self, member):
-    await self.bot.send_message(member, "Hello {} \n"
-                                "👋Welcome👋 to \n"
-                                "**Join 4 Join** server \n"
-                                " \n"
-                                "write **.verify** in <#528209980734832642> to verification✅ your account \n"
-                                "check <#528148347698020353> and read 📝information📝 how to use **Join 4 Join** server and bot \n"
-                                " \n"
-                                "If you need help with something, write **.help** in <#528147248706486282> \n"
-                                "or contact <@&528200802863677450>".format(member.mention)
+    chickenserver = self.bot.get_server("382204136990703616")
+    join4joinserver = self.bot.get_server("382204136990703616")
+    if server in(chickenserver):
+      if member in(join4joinserver.members):
+        await self.bot.send_message(member, "You was verificate your account")
+        return
+      else:
+        return
+    if server in(join4joinserver):
+      await self.bot.send_message(member, "Hello {} \n"
+                                  "👋Welcome👋 to \n"
+                                  "**Join 4 Join** server \n"
+                                  " \n"
+                                  "write **.verify** in <#528209980734832642> to verification✅ your account \n"
+                                  "check <#528148347698020353> and read 📝information📝 how to use **Join 4 Join** server and bot \n"
+                                  " \n"
+                                  "If you need help with something, write **.help** in <#528147248706486282> \n"
+                                  "or contact <@&528200802863677450>".format(member.mention)
     
     
 def setup(bot):
