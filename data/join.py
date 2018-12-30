498c0ea4ae109fd18fa622d934cae2e1a5753ccb
Prisa <43382953+ElPrisa@users.noreply.github.com>
2018-12-30 22:54:42 +0100

    Update join.py

--- data/join.py
+++ data/join.py
@@ -20,7 +20,10 @@ class Join:
     elif member.server.id == "382204136990703616":
       server1 = self.bot.get_server("528142547894272010")
       if member in(server1.members):
-        await self.bot.send_message(discord.Object(id='528146368464945152'), "{} give Verify role pls".format(member.mention))
+        server = self.bot.get_server("528142547894272010")
+        role = discord.utils.get(server.roles, id="528162784022626314")
+        new_member = server.get_member(member.id)
+        await self.bot.add_roles(new_member, role)
         await self.bot.send_message(discord.Object(id='528209980734832642'), "{} was verificate✅".format(member.mention))
         await self.bot.send_message(member, "{} \n"
                                     "Your account was verificate✅ \n"
