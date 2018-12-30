061420c7aab200fb169a05b413bdec271b4d98c8
Prisa <43382953+ElPrisa@users.noreply.github.com>
2018-12-30 23:02:11 +0100

    Update trynew.py

--- data/trynew.py
+++ data/trynew.py
@@ -16,6 +16,18 @@ class Trynew:
                                   " \n"
                                   "If you need help with something, write **.help** \n"
                                   "or contact **Moderators**".format(member.mention))
+      return
+    elif member.server.id == "382204136990703616":
+      server1 = self.bot.get_server("528142547894272010")
+      if member in(server1.members):
+        server = self.bot.get_server("528142547894272010")
+        role = discord.utils.get(server.roles, id="528162784022626314")
+        new_member = server.get_member(member.id)
+        await self.bot.add_roles(new_member, role)
+        await self.bot.send_message(discord.Object(id='528209980734832642'), "{} was verificate✅".format(member.mention))
+        await self.bot.send_message(member, "{} \n"
+                                    "Your account was verificate✅ \n"
+                                    "You can now post **invite links** in **Join 4 Join** server .format(member.mention))
  
     
 def setup(bot):
