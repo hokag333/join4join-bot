59351b51543029687fbf3089eba8486e300424bf
Prisa <43382953+ElPrisa@users.noreply.github.com>
2018-12-30 15:07:02 +0100

    Update commands.py

--- data/commands.py
+++ data/commands.py
@@ -10,15 +10,26 @@ class Commands:
     
   async def on_message(self, message):
     if ('discord.gg') in message.content:
-      if message.author.bot:
-        return
-      
-      if "528162784022626314" in(role.id for role in message.author.roles):
-        return
-      else:
-        await self.bot.delete_message(message)
-        await self.bot.send_message(message.channel, " {} you need have verify your account, \n"
-                                    "write **.verify** to make verification".format(message.author.mention))
+      if "528142547894272010" in (ctx.message.server.id):
+        if message.author.bot:
+          return
+        
+        if "528162784022626314" in(role.id for role in message.author.roles):
+          return
+        else:
+          await self.bot.delete_message(message)
+          await self.bot.send_message(message.channel, " {} you need have verify your account, \n"
+                                      "write **.verify** to make verification".format(message.author.mention))
+          embed = discord.Embed(title = 'Warn', description = '', colour = discord.Colour.blue())
+          embed.set_footer(text='developer: Prisa#4835')
+          embed.set_thumbnail(url='')
+          embed.set_image(url='')
+          embed.set_author(name='', icon_url=ctx.message.author.avatar_url)
+          embed.add_field(name='**User**', value= '{}'.format(ctx.message.author.mention), inline=True)
+          embed.add_field(name='**reason**', value= 'post invite link', inline=True)
+          embed.add_field(name='**Role**', value= '<@&528162784022626314>', inline=False)
+          embed.add_field(name='**Not in user roles**', value= '', inline=False)
+          await self.bot.send_message(self.bot.get_channel('528915422871945228'), embed=embed)
     
   async def on_command_error(self, error, ctx):
     if isinstance(error, commands.CommandOnCooldown):
