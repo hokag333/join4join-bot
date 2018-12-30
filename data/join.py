c846e413da35d27f7658984acbfef95d0f9b5997
Prisa <43382953+ElPrisa@users.noreply.github.com>
2018-12-30 16:17:47 +0100

    Update join.py

--- data/join.py
+++ data/join.py
@@ -6,7 +6,7 @@ class Join:
     self.bot = bot 
                                 
   async def on_member_join(self, member):
-    if "528142547894272010" in(member_join.server.id):
+    if member.server.id("528142547894272010"):
       await self.bot.send_message(member, "{} \n"
                                   "👋Welcome👋 \n"
                                   "to **Join 4 Join** server \n"
@@ -17,11 +17,13 @@ class Join:
                                   "If you need help with something, write **.help** \n"
                                   "or contact <@&528200802863677450>".format(member.mention))
       
-    if "458341394524798976" in(member_join.server.id):
+    if member.server.id("458341394524798976")
       server1 = self.bot.get_server("528142547894272010")
       if member in(server1):
         await self.bot.send_message(member, "{} \n"
                                     " Your account was verificate✅".format(member.mention))
+      else:
+        await self.bot.send_message(member, "test error")
 
     
   async def on_member_remove(self, member):
