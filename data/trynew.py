7f5677d55e45701fd558584ff0af7aae442b2b97
Prisa <43382953+ElPrisa@users.noreply.github.com>
2018-12-30 22:46:24 +0100

    Update trynew.py

--- data/trynew.py
+++ data/trynew.py
@@ -1,1 +1,22 @@
+import discord
+from discord.ext import commands
 
+class Trynew:
+  def __init__(self, bot):
+    self.bot = bot 
+                                
+  async def on_member_join(self, member):
+    if member.server.id == "528142547894272010":
+      await self.bot.send_message(member, "👋Welcome👋 \n"
+                                  "{} \n"
+                                  "to **Join 4 Join** server \n"
+                                  " \n"
+                                  "to **verification**✅ your account write **.verify** in <#528209980734832642> \n"
+                                  "read **information**📝 how to use **Join 4 Join** server and bot in <#528148347698020353>  \n"
+                                  " \n"
+                                  "If you need help with something, write **.help** \n"
+                                  "or contact **Moderators**".format(member.mention))
+ 
+    
+def setup(bot):
+  bot.add_cog(Trynew(bot))
