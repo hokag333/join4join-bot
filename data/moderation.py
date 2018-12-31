a8dd28f17f9e1ad67c816db162a9187400e92c73
Prisa <43382953+ElPrisa@users.noreply.github.com>
2018-12-31 12:32:25 +0100

    Update moderation.py

--- data/moderation.py
+++ data/moderation.py
@@ -33,6 +33,20 @@ class Moderation:
           await self.bot.send_message(discord.Object(id='528915422871945228'), embed=embed)
           await self.bot.send_message(message.channel, " {} you need have **verification account**, \n"
                                       "write **.verify** to make verification".format(message.author.mention))
+          
+  @commands.command(pass_context=True)
+  async def warn(self, ctx, user: discord.User, *, reason=""):
+    mod_list = ["528145435022327810"
+                "528200802863677450"]
+    if mod_list in(role.id for role in ctx.message.author.roles):
+      embed = discord.Embed(title = '__Warn__', description = ' ', color=0xdb781e)
+      embed.set_footer(text='developer: Prisa')
+      embed.set_thumbnail(url=' ')
+      embed.set_image(url=' ')
+      embed.set_author(name=' ', icon_url=user.avatar_url)
+      embed.add_field(name='User', value= '{}'.format(user.mention), inline=True)
+      embed.add_field(name='Reason', value= '{}'.format(reason), inline=True)
+      await self.bot.send_message(user, embed=embed)
     
 def setup(bot):
   bot.add_cog(Moderation(bot))
