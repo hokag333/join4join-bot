275bbf83e477d7d6a1f9e037bfd7511c4ad61bae
Prisa <43382953+ElPrisa@users.noreply.github.com>
2018-12-31 15:25:06 +0100

    Update moderation.py

--- data/moderation.py
+++ data/moderation.py
@@ -26,7 +26,7 @@ class Moderation:
           await self.bot.delete_message(message)
           embed=discord.Embed(title="❗__Warn__", description=" ", color=0xdb781e)
           embed.set_footer(text='developer: Prisa')
-          embed.set_author(name=" ")
+          embed.set_author(name=" ", icon_url=message.author.avatar_url)
           embed.set_image(url=" ")
           embed.add_field(name="User", value=" {} ".format(message.author.mention), inline=True)
           embed.add_field(name="Reason", value="🔗post invite link \n"
@@ -45,7 +45,7 @@ class Moderation:
       await self.bot.delete_message(ctx.message)
       embed=discord.Embed(title="❗__Warn__", description=" ", color=0xdb781e)
       embed.set_footer(text='developer: Prisa')
-      embed.set_author(name=" ")
+      embed.set_author(name=" ", icon_url=user.avatar_url)
       embed.set_image(url=" ")
       embed.add_field(name="👤User", value=" {} ".format(user.mention), inline=True)
       embed.add_field(name="Reason", value="{}".format(reason), inline=True)
