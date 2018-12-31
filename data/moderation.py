53f4c5450a9072c9557b2d551f7503295ea3d3b8
Prisa <43382953+ElPrisa@users.noreply.github.com>
2018-12-31 14:56:53 +0100

    Update moderation.py

--- data/moderation.py
+++ data/moderation.py
@@ -49,6 +49,7 @@ class Moderation:
       embed.set_image(url=" ")
       embed.add_field(name="User", value=" {} ".format(user.mention), inline=True)
       embed.add_field(name="Reason", value="{}".format(reason), inline=True)
+      embed.add_field(name="Moderator", value=" {} ".format(ctx.message.author.mention), inline=True)
       await self.bot.send_message(discord.Object(id='528915422871945228'), embed=embed)
       return
     else:
