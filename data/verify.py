a2f070a0c4a9e925404139ee9765b375df84e1cc
Prisa <43382953+ElPrisa@users.noreply.github.com>
2019-01-01 12:27:31 +0100

    Update verify.py

--- data/verify.py
+++ data/verify.py
@@ -20,6 +20,8 @@ class Verify:
                                   " \n"
                                   "If you need help with something, write **.help** \n"
                                   "or contact **Moderators**".format(member.mention))
+      
+      await self.bot.send_message(discord.Object(id='529619462140461066'), "{} joined **Join 4 Join** server".format(member.mention))
       return
     elif member.server.id == "382204136990703616":
       server1 = self.bot.get_server("528142547894272010")
@@ -63,6 +65,9 @@ class Verify:
         return
       else:
         return
+      
+    elif member.server.id == "528142547894272010":
+      await self.bot.send_message(discord.Object(id='529619462140461066'), "{} left **Join 4 Join** server".format(member.mention))
     else:
       return
       
