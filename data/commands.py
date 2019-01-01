2c99089cede583051184132c5d083bf193e1338f
Prisa <43382953+ElPrisa@users.noreply.github.com>
2019-01-02 00:29:43 +0100

    Update commands.py

--- data/commands.py
+++ data/commands.py
@@ -50,7 +50,7 @@ class Commands:
   async def infodm(self, ctx, *, reason=""):
     if "458341993328803850" in(role.id for role in ctx.message.author.roles):
       await self.bot.delete_message(ctx.message)
-      member = random.choice(ctx.message.server.members)
+      member = random.choice(member in(ctx.message.server.members))
       embed = discord.Embed(title = '**__Information__**', description = ' ', color=0x1378ca)
       embed.set_footer(text='developer: Prisa#4835')
       embed.set_thumbnail(url=' ')
