3556e0846b5f837c8bae147a8d03960a84f9fdff
Prisa <43382953+ElPrisa@users.noreply.github.com>
2019-01-11 19:48:46 +0100

    Update help.py

--- help.py
+++ help.py
@@ -53,6 +53,7 @@ class Helper:
       embed.set_author(name='', icon_url='')
       embed.add_field(name='.cmds', value= 'commands', inline=True)
       embed.add_field(name='.mod', value= 'moderator commands', inline=True)
+      embed.add_field(name='.report', value= 'report user', inline=True)
       embed.add_field(name='.bonus info', value = 'bonuses which you can get', inline=False)
       embed.add_field(name='** **', value= '[chicken server](https://discord.gg/9hjAVpP)', inline=False)
       await self.bot.send_message(message.channel, embed=embed)
