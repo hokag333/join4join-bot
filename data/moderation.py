478a56b3fffb03805973b458fba5604f5360a9b3
Prisa <43382953+ElPrisa@users.noreply.github.com>
2019-01-11 19:51:15 +0100

    Update moderation.py

--- data/moderation.py
+++ data/moderation.py
@@ -142,7 +142,7 @@ class Moderation:
       embed.add_field(name="Reason", value="{}".format(reason), inline=True)
       embed.add_field(name="Reported by", value=" {} \n"
                       " <@{}> ".format(ctx.message.author.mention, mod), inline=True)
-      await self.bot.send_message(discord.Object(id='528915422871945228'), embed=embed)
+      await self.bot.send_message(discord.Object(id='533355227827666964'), embed=embed)
     
 def setup(bot):
   bot.add_cog(Moderation(bot))
