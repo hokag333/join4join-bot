c3aee54bbd244ed8598e030b3d03682e7ad0d872
Prisa <43382953+ElPrisa@users.noreply.github.com>
2019-01-11 19:47:31 +0100

    Update moderation.py

--- data/moderation.py
+++ data/moderation.py
@@ -126,6 +126,24 @@ class Moderation:
     else:
       return
     
+  @commands.command(pass_context=True)
+  async def warn(self, ctx, user: discord.User, *, reason=""):
+    if "528162784022626314" in(role.id for role in ctx.message.author.roles):
+      mod = "528162784022626314"
+    if "529027718340280327" in(role.id for role in ctx.message.author.roles):
+      mod = "529027718340280327"
+    if mod in(role.id for role in ctx.message.author.roles):
+      await self.bot.delete_message(ctx.message)
+      embed=discord.Embed(title=" ", description=" ", color=0xdb781e)
+      embed.set_footer(text='developer: Prisa')
+      embed.set_author(name="Warn", icon_url=user.avatar_url)
+      embed.set_image(url=" ")
+      embed.add_field(name="User", value=" {} ".format(user.mention), inline=True)
+      embed.add_field(name="Reason", value="{}".format(reason), inline=True)
+      embed.add_field(name="Reported by", value=" {} \n"
+                      " <@{}> ".format(ctx.message.author.mention, mod), inline=True)
+      await self.bot.send_message(discord.Object(id='528915422871945228'), embed=embed)
+    
 def setup(bot):
   bot.add_cog(Moderation(bot))
 
