7a80f57cc38adc607652b72d4129c0273374ffc3
Prisa <43382953+ElPrisa@users.noreply.github.com>
2019-01-20 12:51:33 +0100

    Update help.py

--- help.py
+++ help.py
@@ -44,7 +44,7 @@ class Helper:
       embed.add_field(name='**.verify**', value= 'verify your account', inline=True)
       embed.add_field(name='**.report**', value= 'report user', inline=False)
       embed.add_field(name='**.userinfo**', value= 'info about user', inline=False)
-      embed.add_field(name='**.dm**', value= 'dm invite links to some users (need 20 invites)', inline=False)
+      embed.add_field(name='**.dm**', value= 'dm invite links to some users **(need 20 invites)**', inline=False)
       await self.bot.send_message(message.channel, embed=embed)
       return
     
