6dc9323f7a7e8075d9df1bb061e0486382288b2b
Prisa <43382953+ElPrisa@users.noreply.github.com>
2019-02-09 21:02:21 +0100

    Update commands.py

--- data/commands.py
+++ data/commands.py
@@ -65,7 +65,8 @@ class Commands:
                       "bot will invite him to **join4join** server \n"
                       "and invite one random member of **Join4Join** server to your server ", inline=True)
       embed.add_field(name='**fake invites**', value= "if you will making fake invites \n"
-                      "**Partnership** program will be disabled on your server", inline=True)
+                      "**Partnership** program will be disabled on your server \n"
+                      "  ", inline=True)
       embed.add_field(name='**.partnership join**', value= 'invite **join4join** bot to your server', inline=False)
       await self.bot.send_message(ctx.message.channel, embed=embed)
       return
