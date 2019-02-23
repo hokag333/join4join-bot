26406355d3e6647befb1c3583c4b2ad7710c2074
Prisa <43382953+ElPrisa@users.noreply.github.com>
2019-02-23 14:48:05 +0100

    Update verify.py

--- data/verify.py
+++ data/verify.py
@@ -22,7 +22,7 @@ class Verify:
                                   "or contact **Moderators**".format(member.mention))
       
       await self.bot.send_message(discord.Object(id='537918427059453952'), "{} joined **Join 4 Join** server✅ \n"
-                                  "account created at** {} ** ".format(member.mention, member.created_at.strftime(" %j days %y years ")))
+                                  "account created at** {} ** ".format(member.mention, member.created_at.strftime(" %d %B %Y ")))
       return
     elif member.server.id == "537014489598394399":
       server1 = self.bot.get_server("537014667550261318")
