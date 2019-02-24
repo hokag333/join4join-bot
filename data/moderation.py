12a539115adafa6377f79ac8fc9c5a57c2c850ed
Prisa <43382953+ElPrisa@users.noreply.github.com>
2019-02-24 15:34:04 +0100

    Update moderation.py

--- data/moderation.py
+++ data/moderation.py
@@ -19,59 +19,7 @@ class Moderation:
       if "537921215168512000" in(message.channel.id):
         await self.bot.delete_message(message)
         return
-        
-      if ('discord.gg') in message.content:
-        if "537914257639997441" in(role.id for role in message.author.roles):
-          if "537924101227020288" in(message.channel.id):
-            return
-          if "538057195871535135" in(message.channel.id):
-            return
-          
-          await self.bot.delete_message(message)
-          embed=discord.Embed(title=" ", description=" ", color=0xdb781e)
-          embed.set_footer(text='developer: Prisa')
-          embed.set_author(name="Warn", icon_url=message.author.avatar_url)
-          embed.set_image(url=" ")
-          embed.add_field(name="User", value=" {} ".format(message.author.mention), inline=True)
-          embed.add_field(name="Reason", value="🔗post invite link \n"
-                          "in <#{}> ".format(message.channel.id), inline=True)
-          embed.add_field(name="Info", value="message was deleted", inline=True)
-          await self.bot.send_message(discord.Object(id='537919645387194371'), embed=embed)
-          
-          embed=discord.Embed(title=" ", description=" ", color=0xdb781e)
-          embed.set_footer(text='developer: Prisa')
-          embed.set_author(name="Warn", icon_url=message.author.avatar_url)
-          embed.set_image(url=" ")
-          embed.add_field(name="User", value=" {} ".format(message.author.mention), inline=True)
-          embed.add_field(name="Reason", value="🔗post invite link \n"
-                          "please post your invite links only in <#528156791725490186>", inline=True)
-          await self.bot.send_message(message.author, embed=embed)
-          return
-          
-        else:
-          await self.bot.delete_message(message)
-          embed=discord.Embed(title=" ", description=" ", color=0xdb781e)
-          embed.set_footer(text='developer: Prisa')
-          embed.set_author(name="Warn", icon_url=message.author.avatar_url)
-          embed.set_image(url=" ")
-          embed.add_field(name="User", value=" {} ".format(message.author.mention), inline=True)
-          embed.add_field(name="Reason", value="🔗post invite link \n"
-                          "user doesn´t have **Verified** account", inline=True)
-          embed.add_field(name="Info", value="message was deleted", inline=True)
-          await self.bot.send_message(discord.Object(id='537919645387194371'), embed=embed)
-          
-          embed=discord.Embed(title=" ", description=" ", color=0xdb781e)
-          embed.set_footer(text='developer: Prisa')
-          embed.set_author(name="Warn", icon_url=message.author.avatar_url)
-          embed.set_image(url=" ")
-          embed.add_field(name="User", value=" {} ".format(message.author.mention), inline=True)
-          embed.add_field(name="Reason", value="🔗post invite link \n"
-                          "You don´t have **Verified** account ", inline=True)
-          embed.add_field(name="**Info**", value="Just **verify your account** in <#528209980734832642> \n"
-                          "with **.verify** command", inline=False)
-          await self.bot.send_message(message.author, embed=embed)
-          return
-          
+      
       if ('@everyone') in message.content:
         if "538056427374379008" in(role.id for role in message.author.roles):
           return
@@ -123,6 +71,58 @@ class Moderation:
         embed.add_field(name="**Info**", value="just read <#528148347698020353> ", inline=False)
         await self.bot.send_message(message.author, embed=embed)
         return
+      
+      if ('discord.gg') in message.content:
+        if "537914257639997441" in(role.id for role in message.author.roles):
+          if "537924101227020288" in(message.channel.id):
+            return
+          if "538057195871535135" in(message.channel.id):
+            return
+          
+          await self.bot.delete_message(message)
+          embed=discord.Embed(title=" ", description=" ", color=0xdb781e)
+          embed.set_footer(text='developer: Prisa')
+          embed.set_author(name="Warn", icon_url=message.author.avatar_url)
+          embed.set_image(url=" ")
+          embed.add_field(name="User", value=" {} ".format(message.author.mention), inline=True)
+          embed.add_field(name="Reason", value="🔗post invite link \n"
+                          "in <#{}> ".format(message.channel.id), inline=True)
+          embed.add_field(name="Info", value="message was deleted", inline=True)
+          await self.bot.send_message(discord.Object(id='537919645387194371'), embed=embed)
+          
+          embed=discord.Embed(title=" ", description=" ", color=0xdb781e)
+          embed.set_footer(text='developer: Prisa')
+          embed.set_author(name="Warn", icon_url=message.author.avatar_url)
+          embed.set_image(url=" ")
+          embed.add_field(name="User", value=" {} ".format(message.author.mention), inline=True)
+          embed.add_field(name="Reason", value="🔗post invite link \n"
+                          "please post your invite links only in <#528156791725490186>", inline=True)
+          await self.bot.send_message(message.author, embed=embed)
+          return
+          
+        else:
+          await self.bot.delete_message(message)
+          embed=discord.Embed(title=" ", description=" ", color=0xdb781e)
+          embed.set_footer(text='developer: Prisa')
+          embed.set_author(name="Warn", icon_url=message.author.avatar_url)
+          embed.set_image(url=" ")
+          embed.add_field(name="User", value=" {} ".format(message.author.mention), inline=True)
+          embed.add_field(name="Reason", value="🔗post invite link \n"
+                          "user doesn´t have **Verified** account", inline=True)
+          embed.add_field(name="Info", value="message was deleted", inline=True)
+          await self.bot.send_message(discord.Object(id='537919645387194371'), embed=embed)
+          
+          embed=discord.Embed(title=" ", description=" ", color=0xdb781e)
+          embed.set_footer(text='developer: Prisa')
+          embed.set_author(name="Warn", icon_url=message.author.avatar_url)
+          embed.set_image(url=" ")
+          embed.add_field(name="User", value=" {} ".format(message.author.mention), inline=True)
+          embed.add_field(name="Reason", value="🔗post invite link \n"
+                          "You don´t have **Verified** account ", inline=True)
+          embed.add_field(name="**Info**", value="Just **verify your account** in <#528209980734832642> \n"
+                          "with **.verify** command", inline=False)
+          await self.bot.send_message(message.author, embed=embed)
+          return
           
   @commands.command(pass_context=True)
   async def warn(self, ctx, user: discord.User, *, reason=""):
